d98413a9c17a5ff4537b8697b1bdcaeb82f8ef79
sravan kumar <sravanpatel0017@gmail.com>
2023-11-09 21:28:57 +0530

    Added animation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import image from "../images/image2.jpg";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function Home(){
     let navigate=useNavigate();
+    const aboutVariants = {
+        hidden: { opacity: 0},
+        visible: { opacity: 1,transition: { duration: .5 } },
+    };
+    const contactVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { duration: .9} },
+    };
     return(
-        <div className="home">
+        <motion.div  variants={aboutVariants} initial="hidden" animate="visible" className="home">
         <div className="a">
         <h1>Hi There, <br/> I'm <span>Sravan Kumar</span></h1>
         <p>Incididunt ad dolore ex labore excepteur voluptate irure laboris amet id minim non aute excepteur. Aute voluptate sunt ad fugiat. Nisi et nisi ea ad nulla exercitation ea elit. Sit ullamco sint dolor aliqua aute laborum laborum ea ad irure.</p>
@@ -14,6 +23,6 @@ export default function Home(){
         <div className="b">
         <img src={image} alt="image"/>
         </div>
-        </div>
+        </motion.div>
     )
 }
